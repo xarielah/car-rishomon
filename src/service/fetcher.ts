220ca8f1dc2dc1f3139aea523772d4c0e601c275
arielahr <arielah1997@gmail.com>
2023-03-19 16:59:27 +0200

    Url parameters changed

--- src/service/fetcher.ts
+++ src/service/fetcher.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL ?? "";
-const searchCar = (q: string) => axios.get(BASE_URL + "&q=" + q + "&limit=1");
+const searchCar = (q: string) =>
+  axios.get(BASE_URL + `&filters={"mispar_rechev": "` + q + `"}`);
 
 export default searchCar;
